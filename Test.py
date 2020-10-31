d1af156d2fddc890ed19aa39104ea8cd196fdb7b
leoosimas <53009192+leoosimas@users.noreply.github.com>
2020-10-31 02:19:22 -0300

    updates na apliacação para a geração de relatório

--- Test.py
+++ Test.py
@@ -2,7 +2,8 @@ import ldap3
 from os import path
 from getpass import getpass
 import sys
-import pyad
+import json
+import csv
 
 
 
@@ -11,7 +12,21 @@ host = ldap3.Server(host)
 user='administrator@tcclab.com'
 passwd = '1234Qwer'
 
-connection = ldap3.Connection(host, user=user, password=passwd)
-connection.bind()
+conn = ldap3.Connection(host, user=user, password=passwd)
+conn.bind()
 
+if conn.bind() == True:
+    print("Conectado ao Active Directory")
+else:
+    print("Credenciais inválidas, tente novamente")
 
+conn.search('cn=Users,dc=tcclab,dc=com', '(&(objectclass=person))', attributes=ldap3.ALL_ATTRIBUTES)
+
+data = []
+
+for entry in conn.entries:
+   data.append(entry)
+
+with open('report.csv', 'w', newline='') as myfile:
+     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+     wr.writerow(data)
